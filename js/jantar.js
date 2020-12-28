27d9295abcf1614b4520f7805c6e7990f8d25be3
Maria Eduarda <dudaplentz123@gmail.com>
2020-12-28 18:03:08 -0300

    exercício1

---
+++ js/jantar.js
@@ -0,0 +1,9 @@
+var valorinicio;
+function janta() {
+        valorinicio = valor.value;
+        valorinicio = parseFloat(valorinicio);
+        taxagar = valorinicio * 0.1;
+        valortotal = taxagar + valorinicio;
+        outgarçom.innerHTML = taxagar;
+        outtotal.innerHTML = valortotal;
+}
